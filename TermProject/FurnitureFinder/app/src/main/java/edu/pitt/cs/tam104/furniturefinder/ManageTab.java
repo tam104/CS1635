a8858f039b07db139a73558811f9987afc5d22f9
BobXu2358 <xxh2358@gmail.com>
2016-03-21 04:04:10 -0400

    Nearly half way :/
    I am still learning how to do the recyclerView and I’m too dumb to get
    it done for one night…
    However @Tiffany if you got a chance to take a look at the documents of
    recyclerView I feel we could use the same RecyclerView Adapter class
    and the listed item fragment for both our work. Therefore I committed
    my unfinished works anyways.
    Will pick up the work I left soon…
    P.S. Adapter is not yet finished

--- TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/ManageTab.java
+++ TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/ManageTab.java
@@ -2,6 +2,8 @@ package edu.pitt.cs.tam104.furniturefinder;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,15 @@ import android.view.ViewGroup;
  */
 public class ManageTab extends Fragment
 {
+    private RecyclerView r_view;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.manage_layout, container, false);
+
+        r_view = (RecyclerView) view.findViewById(R.id.recycler_view);
+        r_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+        r_view.setAdapter(new RecyclerViewAdapter(getActivity()));
         return view;
     }
+
 }
